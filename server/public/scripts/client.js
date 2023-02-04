4155d88542628f7f01b888886979548e8ed77cdc
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-02-04 09:16:05 -0600

    get post and render functions complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -48,5 +48,50 @@ function render(taskList) {
   renderElement.empty();
 
   for (item of taskList) {
+    let compText =
+      'Task Complete! <input class="compBut" type="button" value="Done!">';
+    if (!item.complete) {
+      compText =
+        'Tackle Me! <input class="compBut" type="button" value="Not done yet!">';
+    }
+    if (item.id == editId) {
+      let appendStr = `
+          <tr data-id=${item.id} data-complete="${item.complete}">
+          <td>
+          <input class='task-in' value="${item.task}">
+          </td>
+          <td>
+          <input class='date-in' value="${item.date}" type="date">
+          </td>
+          <td>
+          ${compText}
+          </td>
+          <td>
+          <span>
+          <input class='accept-btn' type='button' value='Enter Me!'>
+          <input class='cancel-btn' type='button' value='cancel'>
+          </span>
+          </td>`;
+      renderElement.append(appendStr);
+    } else {
+      let appendStr = `
+          <tr data-id=${item.id} data-complete=${item.complete}>
+          <td>
+          ${item.task}
+          </td>
+          <td>
+          ${item.date}
+          </td>
+          <td>
+          ${compText}
+          </td>
+          <td>
+          <span>
+          <input class='edit-btn' type='button' value='Edit Task'>
+          <input class='delete-btn' type='button' value='Remove Task'>
+          </span>
+          </td>`;
+      renderElement.append(appendStr);
+    }
   }
 }
